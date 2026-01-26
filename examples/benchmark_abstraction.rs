bfe5536db85e6f053723f9a3fa65240b5f6cfa0f
Nuttakit Kundum <nuttakitkundum@gmail.com>
2026-01-27 01:13:58 +0700

    feat: update benchmark abstraction with GTO Wizard 50bb configurations

--- examples/benchmark_abstraction.rs
+++ examples/benchmark_abstraction.rs
@@ -23,17 +23,17 @@ fn main() {
     println!("Buckets: {}", num_buckets);
     println!();
 
-    // Same OOP range as baseline
-    let oop_range = "A9s-A3s,A8o:0.906,A7o:0.826,A6o:0.78,A5o:0.49,A4o:0.07,QJs,Q9s-Q3s,QJo,Q9o,Q8o:0.996,Q7o,Q6o:0.99,Q5o:0.92,Q4o:0.916,Q3o:0.866,J9s:0.89,J8s:0.99,J7s-J3s,J9o-J8o,J7o:0.9,J6o:0.98,J5o:0.86,J4o:0.936,J3o:0.98,97s:0.99,96s-93s,96o+,95o:0.936,87s:0.026,86s-83s,85o+,76s:0.946,75s-73s,74o+,63s+,64o+,63o:0.836,53+,43,AhTh:0.37,AdTd:0.37,AcTc:0.37,As2s,Ah2h,Ac2c,KhQh,KdQd,KcQc,KhQs:0.496,KhQd:0.496,KhQc:0.496,KdQs:0.496,KdQh:0.496,KdQc:0.496,KcQs:0.496,KcQh:0.496,KcQd:0.496,KhJh,KdJd,KcJc,KhJs,KhJd,KhJc,KdJs,KdJh,KdJc,KcJs,KcJh,KcJd,KhTh,KdTd,KcTc,KhTd,KhTc,KdTh,KdTc,KcTh,KcTd,Kh9h,Kd9d,Kc9c,Kh9s,Kh9d,Kh9c,Kd9s,Kd9h,Kd9c,Kc9s,Kc9h,Kc9d,Kh8h,Kd8d,Kc8c,Kh8s,Kh8d,Kh8c,Kd8s,Kd8h,Kd8c,Kc8s,Kc8h,Kc8d,Kh7s:0.92,Kh7d:0.92,Kh7c:0.92,Kd7s:0.92,Kd7h:0.92,Kd7c:0.92,Kc7s:0.92,Kc7h:0.92,Kc7d:0.92,Kh6s:0.96,Kh6d:0.96,Kh6c:0.96,Kd6s:0.96,Kd6h:0.96,Kd6c:0.96,Kc6s:0.96,Kc6h:0.96,Kc6d:0.96,Kh5h:0.826,Kd5d:0.826,Kc5c:0.826,Kh5s:0.95,Kh5d:0.95,Kh5c:0.95,Kd5s:0.95,Kd5h:0.95,Kd5c:0.95,Kc5s:0.95,Kc5h:0.95,Kc5d:0.95,Kh4h,Kd4d,Kc4c,Kh4s:0.9,Kh4d:0.9,Kh4c:0.9,Kd4s:0.9,Kd4h:0.9,Kd4c:0.9,Kc4s:0.9,Kc4h:0.9,Kc4d:0.9,Kh3h,Kd3d,Kc3c,Kh3s:0.92,Kh3d:0.92,Kh3c:0.92,Kd3s:0.92,Kd3h:0.92,Kd3c:0.92,Kc3s:0.92,Kc3h:0.92,Kc3d:0.92,Kh2h,Kc2c,Kh2s:0.966,Kh2c:0.966,Kd2s:0.966,Kd2h:0.966,Kd2c:0.966,Kc2s:0.966,Kc2h:0.966,QhTh,QdTd,QcTc,QsTh,QsTd,QsTc,QhTd,QhTc,QdTh,QdTc,QcTh,QcTd,Qs2s,Qh2h,Qc2c,Qs2h:0.906,Qs2c:0.906,Qh2s:0.906,Qh2c:0.906,Qd2s:0.906,Qd2h:0.906,Qd2c:0.906,Qc2s:0.906,Qc2h:0.906,JhTh:0.686,JdTd:0.686,JcTc:0.686,JsTh,JsTd,JsTc,JhTd,JhTc,JdTh,JdTc,JcTh,JcTd,Js2s,Jh2h,Jc2c,Js2h:0.986,Js2c:0.986,Jh2s:0.986,Jh2c:0.986,Jd2s:0.986,Jd2h:0.986,Jd2c:0.986,Jc2s:0.986,Jc2h:0.986,Th9h:0.026,Td9d:0.026,Tc9c:0.026,Th9s,Th9d,Th9c,Td9s,Td9h,Td9c,Tc9s,Tc9h,Tc9d,Th8h:0.376,Td8d:0.376,Tc8c:0.376,Th8s,Th8d,Th8c,Td8s,Td8h,Td8c,Tc8s,Tc8h,Tc8d,Th7h,Td7d,Tc7c,Th7s,Th7d,Th7c,Td7s,Td7h,Td7c,Tc7s,Tc7h,Tc7d,Th6h,Td6d,Tc6c,Th6s:0.866,Th6d:0.866,Th6c:0.866,Td6s:0.866,Td6h:0.866,Td6c:0.866,Tc6s:0.866,Tc6h:0.866,Tc6d:0.866,Th5h,Td5d,Tc5c,Th5s:0.49,Th5d:0.49,Th5c:0.49,Td5s:0.49,Td5h:0.49,Td5c:0.49,Tc5s:0.49,Tc5h:0.49,Tc5d:0.49,Th4h,Td4d,Tc4c,Th4s:0.46,Th4d:0.46,Th4c:0.46,Td4s:0.46,Td4h:0.46,Td4c:0.46,Tc4s:0.46,Tc4h:0.46,Tc4d:0.46,Th3h,Td3d,Tc3c,Th2h,Tc2c,9s2s,9h2h,9c2c,8s2s,8h2h,8c2c,7s2s,7h2h,7c2c,6s2s,6h2h,6c2c,5s2s,5h2h,5c2c,5s2h:0.58,5s2c:0.58,5h2s:0.58,5h2c:0.58,5d2s:0.58,5d2h:0.58,5d2c:0.58,5c2s:0.58,5c2h:0.58,4s2s,4h2h,4c2c,3s2s,3h2h,3c2c";
+    // OOP range from GTO Wizard 50bb config
+    let oop_range = "66:0.09,55:0.766,44:0.91,33:0.716,22:0.526,KJs:0.32,K9s-K2s,KJo:0.76,K9o:0.976,K8o:0.986,K7o:0.666,K6o:0.47,K5o:0.73,K4o-K3o:0.976,K2o,J9s-J8s,J7s:0.396,J6s-J2s,J9o:0.816,J8o:0.77,J7o:0.99,J6o-J5o,J4o:0.646,98s:0.36,97s:0.326,96s:0.47,95s-92s,98o:0.92,97o-96o,87s:0.476,86s:0.52,85s-82s,86o+,76s:0.71,75s-72s,75o+,62s+,65o,64o:0.996,52s+,53o+,42s+,43o:0.256,32s,AsJh:0.236,AsJd:0.236,AsJc:0.236,AdJs:0.236,AdJh:0.236,AdJc:0.236,AcJs:0.236,AcJh:0.236,AcJd:0.236,AsTs,AdTd,AsTh:0.136,AsTd:0.136,AdTs:0.136,AdTh:0.136,AcTs:0.136,AcTh:0.136,AcTd:0.136,As9s,Ad9d,Ac9c,As9h:0.78,As9d:0.78,As9c:0.78,Ad9s:0.78,Ad9h:0.78,Ad9c:0.78,Ac9s:0.78,Ac9h:0.78,Ac9d:0.78,As8s,Ad8d,Ac8c,As8h:0.98,As8d:0.98,As8c:0.98,Ad8s:0.98,Ad8h:0.98,Ad8c:0.98,Ac8s:0.98,Ac8h:0.98,Ac8d:0.98,As7s,Ad7d,Ac7c,As7h:0.706,As7d:0.706,As7c:0.706,Ad7s:0.706,Ad7h:0.706,Ad7c:0.706,Ac7s:0.706,Ac7h:0.706,Ac7d:0.706,As6s,Ad6d,Ac6c,As6h:0.74,As6d:0.74,As6c:0.74,Ad6s:0.74,Ad6h:0.74,Ad6c:0.74,Ac6s:0.74,Ac6h:0.74,Ac6d:0.74,As5s,Ad5d,Ac5c,As5h:0.676,As5d:0.676,As5c:0.676,Ad5s:0.676,Ad5h:0.676,Ad5c:0.676,Ac5s:0.676,Ac5h:0.676,Ac5d:0.676,As4s,Ad4d,Ac4c,As4h:0.7,As4d:0.7,As4c:0.7,Ad4s:0.7,Ad4h:0.7,Ad4c:0.7,Ac4s:0.7,Ac4h:0.7,Ac4d:0.7,As3s,Ad3d,Ac3c,As3h:0.836,As3d:0.836,As3c:0.836,Ad3s:0.836,Ad3h:0.836,Ad3c:0.836,Ac3s:0.836,Ac3h:0.836,Ac3d:0.836,As2s,Ad2d,Ac2c,As2h:0.656,As2d:0.656,As2c:0.656,Ad2s:0.656,Ad2h:0.656,Ad2c:0.656,Ac2s:0.656,Ac2h:0.656,Ac2d:0.656,KsQh:0.556,KsQc:0.556,KhQs:0.556,KhQc:0.556,KdQs:0.556,KdQh:0.556,KdQc:0.556,KcQs:0.556,KcQh:0.556,KsTs,KhTh,KdTd,KsTh:0.826,KsTd:0.826,KhTs:0.826,KhTd:0.826,KdTs:0.826,KdTh:0.826,KcTs:0.826,KcTh:0.826,KcTd:0.826,QsJs:0.99,QhJh:0.99,QcJc:0.99,QsJh:0.73,QsJd:0.73,QsJc:0.73,QhJs:0.73,QhJd:0.73,QhJc:0.73,QcJs:0.73,QcJh:0.73,QcJd:0.73,QsTs,QhTh,QsTh:0.896,QsTd:0.896,QhTs:0.896,QhTd:0.896,QcTs:0.896,QcTh:0.896,QcTd:0.896,Qs9s,Qh9h,Qc9c,Qs9h:0.866,Qs9d:0.866,Qs9c:0.866,Qh9s:0.866,Qh9d:0.866,Qh9c:0.866,Qc9s:0.866,Qc9h:0.866,Qc9d:0.866,Qs8s,Qh8h,Qc8c,Qs8h:0.84,Qs8d:0.84,Qs8c:0.84,Qh8s:0.84,Qh8d:0.84,Qh8c:0.84,Qc8s:0.84,Qc8h:0.84,Qc8d:0.84,Qs7s,Qh7h,Qc7c,Qs7h:0.906,Qs7d:0.906,Qs7c:0.906,Qh7s:0.906,Qh7d:0.906,Qh7c:0.906,Qc7s:0.906,Qc7h:0.906,Qc7d:0.906,Qs6s,Qh6h,Qc6c,Qs6h:0.67,Qs6d:0.67,Qs6c:0.67,Qh6s:0.67,Qh6d:0.67,Qh6c:0.67,Qc6s:0.67,Qc6h:0.67,Qc6d:0.67,Qs5s:0.496,Qh5h:0.496,Qc5c:0.496,Qs5h:0.9,Qs5d:0.9,Qs5c:0.9,Qh5s:0.9,Qh5d:0.9,Qh5c:0.9,Qc5s:0.9,Qc5h:0.9,Qc5d:0.9,Qs4s,Qh4h,Qc4c,Qs4h:0.97,Qs4d:0.97,Qs4c:0.97,Qh4s:0.97,Qh4d:0.97,Qh4c:0.97,Qc4s:0.97,Qc4h:0.97,Qc4d:0.97,Qs3s:0.926,Qh3h:0.926,Qc3c:0.926,Qs3h,Qs3d,Qs3c,Qh3s,Qh3d,Qh3c,Qc3s,Qc3h,Qc3d,Qs2s,Qh2h,Qc2c,Qs2h:0.876,Qs2d:0.876,Qs2c:0.876,Qh2s:0.876,Qh2d:0.876,Qh2c:0.876,Qc2s:0.876,Qc2h:0.876,Qc2d:0.876,JsTs,JhTh,JdTd,JsTh:0.98,JsTd:0.98,JhTs:0.98,JhTd:0.98,JdTs:0.98,JdTh:0.98,JcTs:0.98,JcTh:0.98,JcTd:0.98,Ts9s,Th9h,Td9d,Ts9h:0.786,Ts9d:0.786,Ts9c:0.786,Th9s:0.786,Th9d:0.786,Th9c:0.786,Td9s:0.786,Td9h:0.786,Td9c:0.786,Ts8s,Th8h,Td8d,Ts8h:0.76,Ts8d:0.76,Ts8c:0.76,Th8s:0.76,Th8d:0.76,Th8c:0.76,Td8s:0.76,Td8h:0.76,Td8c:0.76,Ts7s:0.35,Th7h:0.35,Td7d:0.35,Ts7h:0.986,Ts7d:0.986,Ts7c:0.986,Th7s:0.986,Th7d:0.986,Th7c:0.986,Td7s:0.986,Td7h:0.986,Td7c:0.986,Ts6s:0.366,Th6h:0.366,Td6d:0.366,Ts6h,Ts6d,Ts6c,Th6s,Th6d,Th6c,Td6s,Td6h,Td6c,Ts5s,Th5h,Td5d,Ts4s,Th4h,Td4d,Ts3s,Th3h,Td3d,Ts2s,Th2h,Td2d";
 
-    // Same IP range as baseline
-    let ip_range = "AA:0.826,QQ:0.876,JJ:0.816,99:0.72,88:0.846,77:0.946,66:0.93,55:0.82,AQs:0.766,AJs:0.69,A9s,A8s:0.916,A7s:0.486,A6s:0.466,A5s:0.26,AQo:0.786,AJo:0.78,A9o:0.136,A8o:0.09,A7o:0.306,A6o:0.946,A5o:0.72,A4o:0.67,A3o:0.586,QJs,Q9s:0.3,Q8s:0.65,Q7s:0.75,Q6s:0.89,Q5s:0.95,Q4s:0.63,QJo:0.62,Q9o:0.746,Q8o:0.36,J9s:0.5,J8s:0.816,J7s:0.91,J6s:0.75,J5s:0.59,J4s:0.2,J9o:0.73,J8o:0.336,98s:0.806,97s:0.8,96s:0.666,98o:0.4,87s:0.63,86s:0.61,85s:0.746,76s:0.666,75s:0.556,65s:0.496,54s:0.42,KhKd:0.846,KhKc:0.846,KdKc:0.846,ThTd:0.73,ThTc:0.73,TdTc:0.73,AhKh:0.96,AdKd:0.96,AcKc:0.96,AsKh:0.8,AsKd:0.8,AsKc:0.8,AhKd:0.8,AhKc:0.8,AdKh:0.8,AdKc:0.8,AcKh:0.8,AcKd:0.8,AhTh:0.516,AdTd:0.516,AcTc:0.516,AsTh:0.8,AsTd:0.8,AsTc:0.8,AhTd:0.8,AhTc:0.8,AdTh:0.8,AdTc:0.8,AcTh:0.8,AcTd:0.8,As2h:0.24,As2c:0.24,Ah2s:0.24,Ah2c:0.24,Ad2s:0.24,Ad2h:0.24,Ad2c:0.24,Ac2s:0.24,Ac2h:0.24,KhQh:0.84,KdQd:0.84,KcQc:0.84,KhQs:0.58,KhQd:0.58,KhQc:0.58,KdQs:0.58,KdQh:0.58,KdQc:0.58,KcQs:0.58,KcQh:0.58,KcQd:0.58,KhJh:0.916,KdJd:0.916,KcJc:0.916,KhJs:0.516,KhJd:0.516,KhJc:0.516,KdJs:0.516,KdJh:0.516,KdJc:0.516,KcJs:0.516,KcJh:0.516,KcJd:0.516,KhTh:0.346,KdTd:0.346,KcTc:0.346,KhTd:0.826,KhTc:0.826,KdTh:0.826,KdTc:0.826,KcTh:0.826,KcTd:0.826,Kh9h:0.276,Kd9d:0.276,Kc9c:0.276,Kh9s:0.736,Kh9d:0.736,Kh9c:0.736,Kd9s:0.736,Kd9h:0.736,Kd9c:0.736,Kc9s:0.736,Kc9h:0.736,Kc9d:0.736,Kh8h:0.44,Kd8d:0.44,Kc8c:0.44,Kh8s:0.73,Kh8d:0.73,Kh8c:0.73,Kd8s:0.73,Kd8h:0.73,Kd8c:0.73,Kc8s:0.73,Kc8h:0.73,Kc8d:0.73,Kh7h:0.486,Kd7d:0.486,Kc7c:0.486,Kh7s:0.586,Kh7d:0.586,Kh7c:0.586,Kd7s:0.586,Kd7h:0.586,Kd7c:0.586,Kc7s:0.586,Kc7h:0.586,Kc7d:0.586,Kh6h:0.686,Kd6d:0.686,Kc6c:0.686,Kh5h:0.926,Kd5d:0.926,Kc5c:0.926,Kh4h:0.89,Kd4d:0.89,Kc4c:0.89,Kh3h:0.566,Kd3d:0.566,Kc3c:0.566,QhTh:0.176,QdTd:0.176,QcTc:0.176,QsTh:0.756,QsTd:0.756,QsTc:0.756,QhTd:0.756,QhTc:0.756,QdTh:0.756,QdTc:0.756,QcTh:0.756,QcTd:0.756,JsTh:0.81,JsTd:0.81,JsTc:0.81,JhTd:0.81,JhTc:0.81,JdTh:0.81,JdTc:0.81,JcTh:0.81,JcTd:0.81,Th9h:0.56,Td9d:0.56,Tc9c:0.56,Th9s:0.7,Th9d:0.7,Th9c:0.7,Td9s:0.7,Td9h:0.7,Td9c:0.7,Tc9s:0.7,Tc9h:0.7,Tc9d:0.7,Th8h:0.84,Td8d:0.84,Tc8c:0.84,Th8s:0.3,Th8d:0.3,Th8c:0.3,Td8s:0.3,Td8h:0.3,Td8c:0.3,Tc8s:0.3,Tc8h:0.3,Tc8d:0.3,Th7h:0.81,Td7d:0.81,Tc7c:0.81,Th6h:0.5,Td6d:0.5,Tc6c:0.5";
+    // IP range from GTO Wizard 50bb config
+    let ip_range = "KK,JJ,99-22,KJs,K9s-K2s,KJo,K9o-K6o,K5o:0.36,J9s-J2s,J9o-J8o,95s+,98o,97o:0.52,85s+,87o,74s+,64s+,53s+,AsAd,AsAc,AdAc,QsQh,QsQc,QhQc,TsTh,TsTd,ThTd,AsKs,AdKd,AcKc,AsKh,AsKd,AsKc,AdKs,AdKh,AdKc,AcKs,AcKh,AcKd,AsQs,AcQc,AsQh,AsQc,AdQs,AdQh,AdQc,AcQs,AcQh,AsJs,AdJd,AcJc,AsJh,AsJd,AsJc,AdJs,AdJh,AdJc,AcJs,AcJh,AcJd,AsTs,AdTd,AsTh,AsTd,AdTs,AdTh,AcTs,AcTh,AcTd,As9s,Ad9d,Ac9c,As9h,As9d,As9c,Ad9s,Ad9h,Ad9c,Ac9s,Ac9h,Ac9d,As8s,Ad8d,Ac8c,As8h,As8d,As8c,Ad8s,Ad8h,Ad8c,Ac8s,Ac8h,Ac8d,As7s,Ad7d,Ac7c,As7h,As7d,As7c,Ad7s,Ad7h,Ad7c,Ac7s,Ac7h,Ac7d,As6s,Ad6d,Ac6c,As6h,As6d,As6c,Ad6s,Ad6h,Ad6c,Ac6s,Ac6h,Ac6d,As5s,Ad5d,Ac5c,As5h,As5d,As5c,Ad5s,Ad5h,Ad5c,Ac5s,Ac5h,Ac5d,As4s,Ad4d,Ac4c,As4h,As4d,As4c,Ad4s,Ad4h,Ad4c,Ac4s,Ac4h,Ac4d,As3s,Ad3d,Ac3c,As3h,As3d,As3c,Ad3s,Ad3h,Ad3c,Ac3s,Ac3h,Ac3d,As2s,Ad2d,Ac2c,As2h,As2d,As2c,Ad2s,Ad2h,Ad2c,Ac2s,Ac2h,Ac2d,KsQs,KhQh,KcQc,KsQh,KsQc,KhQs,KhQc,KdQs,KdQh,KdQc,KcQs,KcQh,KsTs,KhTh,KdTd,KsTh,KsTd,KhTs,KhTd,KdTs,KdTh,KcTs,KcTh,KcTd,QsJs,QhJh,QcJc,QsJh,QsJd,QsJc,QhJs,QhJd,QhJc,QcJs,QcJh,QcJd,QsTs,QhTh,QsTh,QsTd,QhTs,QhTd,QcTs,QcTh,QcTd,Qs9s,Qh9h,Qc9c,Qs9h,Qs9d,Qs9c,Qh9s,Qh9d,Qh9c,Qc9s,Qc9h,Qc9d,Qs8s,Qh8h,Qc8c,Qs8h,Qs8d,Qs8c,Qh8s,Qh8d,Qh8c,Qc8s,Qc8h,Qc8d,Qs7s,Qh7h,Qc7c,Qs7h:0.97,Qs7d:0.97,Qs7c:0.97,Qh7s:0.97,Qh7d:0.97,Qh7c:0.97,Qc7s:0.97,Qc7h:0.97,Qc7d:0.97,Qs6s,Qh6h,Qc6c,Qs5s,Qh5h,Qc5c,Qs4s,Qh4h,Qc4c,Qs3s,Qh3h,Qc3c,Qs2s,Qh2h,Qc2c,JsTs,JhTh,JdTd,JsTh,JsTd,JhTs,JhTd,JdTs,JdTh,JcTs,JcTh,JcTd,Ts9s,Th9h,Td9d,Ts9h,Ts9d,Ts9c,Th9s,Th9d,Th9c,Td9s,Td9h,Td9c,Ts8s,Th8h,Td8d,Ts8h,Ts8d,Ts8c,Th8s,Th8d,Th8c,Td8s,Td8h,Td8c,Ts7s,Th7h,Td7d,Ts7h:0.36,Ts7d:0.36,Ts7c:0.36,Th7s:0.36,Th7d:0.36,Th7c:0.36,Td7s:0.36,Td7h:0.36,Td7c:0.36,Ts6s,Th6h,Td6d,Ts5s,Th5h,Td5d,Ts4s,Th4h,Td4d,Ts3s,Th3h,Td3d";
 
     // Parse ranges
     let oop: Range = oop_range.parse().expect("Failed to parse OOP range");
     let ip: Range = ip_range.parse().expect("Failed to parse IP range");
 
-    // Same card config as baseline
+    // Card config - starting from flop (Td9d6h same as before for comparison)
     let card_config = CardConfig {
         range: [oop, ip],
         flop: flop_from_str("Td9d6h").unwrap(),
@@ -41,24 +41,43 @@ fn main() {
         river: NOT_DEALT,
     };
 
-    // Same bet sizes as baseline
-    let bet_sizes = BetSizeOptions::try_from(("25%, 50%, 100%", "50%")).unwrap();
+    // GTO Wizard 50bb bet sizes
+    // OOP: bet "33, a", raise "33, 55, 83, 125, a"
+    // IP: bet "20, 33, 55, 83, 125, a", raise "33, 55, 83, 125, a"
+    let oop_flop_bet = BetSizeOptions::try_from(("33%, a", "33%, 55%, 83%, 125%, a")).unwrap();
+    let ip_flop_bet = BetSizeOptions::try_from(("20%, 33%, 55%, 83%, 125%, a", "33%, 55%, 83%, 125%, a")).unwrap();
 
-    // Same tree config as baseline
+    // Turn: OOP bet "20, 33, 55, 83, 125, 200, a", raise "33, 55, 83, 125, a"
+    //       IP bet "20, 33, 55, 83, 125, 200, a", raise "33, 55, 83, 125, a"
+    let oop_turn_bet = BetSizeOptions::try_from(("20%, 33%, 55%, 83%, 125%, 200%, a", "33%, 55%, 83%, 125%, a")).unwrap();
+    let ip_turn_bet = BetSizeOptions::try_from(("20%, 33%, 55%, 83%, 125%, 200%, a", "33%, 55%, 83%, 125%, a")).unwrap();
+
+    // River: OOP bet "11, 35, 60, 85, 149, a", raise "35, 55, 126, a"
+    //        IP bet "11, 35, 60, 85, 149, a", raise "35, 56, 126, a"
+    let oop_river_bet = BetSizeOptions::try_from(("11%, 35%, 60%, 85%, 149%, a", "35%, 55%, 126%, a")).unwrap();
+    let ip_river_bet = BetSizeOptions::try_from(("11%, 35%, 60%, 85%, 149%, a", "35%, 56%, 126%, a")).unwrap();
+
+    // Donk sizes
+    // Turn donk: "20, 50, a"
+    // River donk: "11, 35, 60, 85, 149, a"
+    let turn_donk = DonkSizeOptions::try_from("20%, 50%, a").ok();
+    let river_donk = DonkSizeOptions::try_from("11%, 35%, 60%, 85%, 149%, a").ok();
+
+    // Tree config from GTO Wizard 50bb
     let tree_config = TreeConfig {
         initial_state: BoardState::Flop,
-        starting_pot: 55,
-        effective_stack: 180,
+        starting_pot: 6,
+        effective_stack: 47,
         rake_rate: 0.0,
         rake_cap: 0.0,
-        flop_bet_sizes: [bet_sizes.clone(), bet_sizes.clone()],
-        turn_bet_sizes: [bet_sizes.clone(), bet_sizes.clone()],
-        river_bet_sizes: [bet_sizes.clone(), bet_sizes],
-        turn_donk_sizes: None,
-        river_donk_sizes: None,
-        add_allin_threshold: 1.5,
-        force_allin_threshold: 0.2,
-        merging_threshold: 0.1,
+        flop_bet_sizes: [oop_flop_bet, ip_flop_bet],
+        turn_bet_sizes: [oop_turn_bet, ip_turn_bet],
+        river_bet_sizes: [oop_river_bet, ip_river_bet],
+        turn_donk_sizes: turn_donk,
+        river_donk_sizes: river_donk,
+        add_allin_threshold: 1.5,       // 150%
+        force_allin_threshold: 0.2,     // 20%
+        merging_threshold: 0.1,         // 10%
     };
 
     // Build action tree
@@ -111,13 +130,13 @@ fn main() {
     println!("Memory allocated in {:.2}s", alloc_start.elapsed().as_secs_f64());
     println!();
 
-    // Same solve parameters as baseline
-    let max_iterations = 1000;
-    let target_exploitability = game.tree_config().starting_pot as f32 * 0.005; // 0.5% of pot
+    // Target exploitability: 0.08
+    let max_iterations = 10000;
+    let target_exploitability = 0.08;
 
     println!("=== Solving ===");
     println!("Max iterations: {}", max_iterations);
-    println!("Target exploitability: {:.4} (0.5% of pot)", target_exploitability);
+    println!("Target exploitability: {:.4}", target_exploitability);
     println!();
 
     // Solve
@@ -133,31 +152,13 @@ fn main() {
     println!("Exploitability as % of pot: {:.3}%", exploitability / game.tree_config().starting_pot as f32 * 100.0);
     println!();
 
-    // Comparison with baseline
-    println!("=== Comparison with Baseline ===");
-    let baseline_time = 84.38; // From baseline_results.txt
-    let baseline_exploitability = 0.2477;
-    let baseline_memory = 11728.68; // MB
-
-    println!("                    Baseline    Abstracted    Change");
-    println!("Solve time:         {:.2}s       {:.2}s         {:.1}x",
-             baseline_time, solve_time.as_secs_f64(),
-             baseline_time / solve_time.as_secs_f64());
-    println!("Exploitability:     {:.4}      {:.4}        {:.1}x",
-             baseline_exploitability, exploitability,
-             exploitability / baseline_exploitability);
-    println!("Memory:             {:.2} MB   {:.2} MB     -{:.1}%",
-             baseline_memory, mem_uncompressed_abs as f64 / 1024.0 / 1024.0,
-             memory_reduction);
-    println!();
-
     // Summary
     println!("=== Summary ===");
-    if solve_time.as_secs_f64() < baseline_time {
-        println!("Speed: {:.1}x FASTER", baseline_time / solve_time.as_secs_f64());
-    } else {
-        println!("Speed: {:.1}x slower", solve_time.as_secs_f64() / baseline_time);
-    }
-    println!("Memory: {:.1}% reduction", memory_reduction);
-    println!("Exploitability ratio: {:.2}x baseline", exploitability / baseline_exploitability);
+    println!("Config: GTO Wizard 50bb");
+    println!("Flop: Td9d6h");
+    println!("Buckets: {}", num_buckets);
+    println!("Threads: {}", rayon::current_num_threads());
+    println!("Memory reduction: {:.1}%", memory_reduction);
+    println!("Solve time: {:.2}s", solve_time.as_secs_f64());
+    println!("Exploitability: {:.4}", exploitability);
 }
